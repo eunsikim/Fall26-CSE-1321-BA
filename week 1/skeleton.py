01470e192c3a69dd4f018b5367ace4b236ec038e
eunsikim <kimeunsik229@gmail.com>
2026-08-28 11:45:46 -0400

    in class 08/28

---
+++ week 1/skeleton.py
@@ -0,0 +1,5 @@
+def main():
+    # Start writing code here...
+
+if __name__ == "__main__":
+    main()
---
+++ week 1/variables.py
@@ -0,0 +1,11 @@
+def main():
+    name1 = "Eun Sik"
+    name2 = "John"
+    user_age = 20
+
+    print(name1)
+    print(name2)
+    print(user_age)
+
+if __name__ == "__main__":
+    main()
