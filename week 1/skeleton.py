8edca5652c5375f453690e0c45f30410ba99de1e
eunsikim <kimeunsik229@gmail.com>
2026-08-31 10:09:33 -0400

    Updated commments from 08/28

--- week 1/skeleton.py
+++ week 1/skeleton.py
@@ -1,3 +1,7 @@
+# This skeleton code uses some concepts we have
+# not covered yet, we will eventually talk about
+# these, but for now please use this "template"
+# whenever you start writing code.
 def main():
     # Start writing code here...
 
---
+++ week 1/variables_and_print.py
@@ -0,0 +1,17 @@
+def main():
+    # We can create variables by defining
+    # its name/identifier and assign (=) a
+    # value to it.
+    name1 = "Eun Sik"
+    name2 = "John"
+    user_age = 20
+
+    # We can command the computer to 
+    # write a message or output by
+    # using the print function.
+    print(name1) # The value between parentheses is the value the computer will "print"
+    print(name2)
+    print(user_age)
+
+if __name__ == "__main__":
+    main()
