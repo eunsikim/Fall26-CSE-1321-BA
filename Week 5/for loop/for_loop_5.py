7f033aea30fddc2582f65dd3cf200f22e6b89fc8
Kim Eun Sik <kimeunsik229@gmail.com>
2026-09-23 15:15:42 +0000

    in class 09/23

---
+++ Week 5/for loop/for_loop_5.py
@@ -0,0 +1,14 @@
+# Password Validator
+# A valid password must:
+# Be at least 8 characters long
+# Must contain at least 1 special character: !, @, #
+# Must contain at least 1 number
+# 
+# The program should ask the user for a password
+# then output if the password is valid or not
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
